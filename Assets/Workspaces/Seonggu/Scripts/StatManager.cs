5cdf0cbb62ba32ed37f5924a780323affe1d5fdd
hongseonggu <hongsam1219@gmail.com>
2026-09-09 00:23:30 +0900

    wip: 서버에서 몬스터 정보가져와서 생성하는 기능 작업중
    작업중

--- Assets/Workspaces/Seonggu/Scripts/StatManager.cs
+++ Assets/Workspaces/Seonggu/Scripts/StatManager.cs
@@ -56,12 +56,6 @@ public class StatManager : MonoBehaviour
             runtimePlayerStat = Instantiate(originalPlayerStat);
             runtimePlayerStat.ResetStat();
         }
-
-        if (originalEnemyStat != null)
-        {
-            runtimeEnemyStat = Instantiate(originalEnemyStat);
-            runtimeEnemyStat.ResetStat();
-        }
     }
 
     // BattleFactory가 호출할 스탯 제공 메서드
@@ -74,6 +68,14 @@ public class StatManager : MonoBehaviour
     public UnitStatData GetEnemyStat()
     {
         return runtimeEnemyStat;
+    }
+
+    public void SetEnemyStat(int hp, int shield)
+    {
+        runtimeEnemyStat.maxHp = hp;
+        runtimeEnemyStat.currentHp = hp;
+        runtimeEnemyStat.maxShield = shield;
+        runtimeEnemyStat.currentShield = 0;
     }
 
     // 전투 종료 후 플레이어 상태를 저장하거나 갱신할 때 사용
